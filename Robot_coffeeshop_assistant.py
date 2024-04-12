b027db5d121e78fc1ab16b2fa18b50ad9ddc3b40
Kosi07 <kosiossai07@gmail.com>
2024-04-12 10:17:18 +0200

    Meow

--- Robot_coffeeshop_assistant.py
+++ Robot_coffeeshop_assistant.py
@@ -5,7 +5,22 @@ print("Hello and Welcome to our Coffee Shop!!!!!!")
 idiot = input("Nigga what tf is your name?!!\n")
 
 if idiot == "Ben":
-    print("Get TF out Evil Ben!" )
+ Ans = input("Are you evil, yes or no?\n")
+ if Ans == "yes":
+   print("Get TF out Evil Ben!" ) 
+ else:
+   print("What would you like to order?")
+   menu = input("Coffee or Tea?\n")
+
+   quantity = input("How many cups would you like?\n")
+
+   print("Hello " + idiot + ". Thanks for coming :)\n")
+
+   print("Your " + menu + " will be ready in 5 minutes ;)")
+
+   price = 8
+   total = price * int(quantity)
+   print("Your total comes down to " + "$" + str(total))
 else:
   #Asking for customer's orders
    print("What would you like to order?")
